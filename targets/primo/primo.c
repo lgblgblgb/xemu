7d47551f3cc886ac483a50b01efb3c952e7c22ec
LGB <lgblgblgb@gmail.com>
2016-10-01 19:11:38 +0200

    Primo: ported to HID, some keymapping

--- targets/primo/primo.c
+++ targets/primo/primo.c
@@ -18,11 +18,13 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#include <stdio.h>
-#include <SDL.h>
 #include "emutools.h"
+#include "emutools_hid.h"
 #include "z80.h"
 #include "primo.h"
+
+
+#define CLOCKS_PER_FRAME (CPU_CLOCK / 50)
 
 
 static Uint8 memory[0x10000];
@@ -31,7 +33,6 @@ static const Uint8 init_primo_palette_rgb[2 * 3] = {	// Primo is simple enough, 
 	0xFF, 0xFF, 0xFF	// white
 };
 static Uint32 primo_palette[2];
-static Uint64 keyboard_state;
 static int frameskip = 0;
 static int primo_screen = 0xC800;
 static int enable_nmi = 0;
@@ -57,7 +58,8 @@ void z80ex_mwrite_cb ( Z80EX_WORD addr, Z80EX_BYTE value )
 Z80EX_BYTE z80ex_pread_cb ( Z80EX_WORD port16 )
 {
 	if ((port16 & 0xFF) < 0x40) {
-		return ((keyboard_state >> (port16 & 0x3F)) & 1) | vsync;
+		// See the comments at the "matrix" definition below
+		return (((~kbd_matrix[(port16 >> 3) & 7]) >> (port16 & 7)) & 1) | vsync;
 	} else
 		return 0xFF;
 }
@@ -84,86 +86,85 @@ void z80ex_reti_cb ( void )
 
 
 
+#define VIRTUAL_SHIFT_POS	0x03
 
-struct KeyMapping {
-	SDL_Scancode	scan;		// SDL scancode for the given key we want to map
-	Uint8		pos;		// BCD packed, high nibble / low nibble for col/row to map to.  0xFF means end of table!, high bit set on low nibble: press virtual shift as well!
-};
-static const struct KeyMapping key_map[] = {
-	{ SDL_SCANCODE_1,		0x00 }, // 1
-	{ SDL_SCANCODE_3,		0x01 }, // 3
-	{ SDL_SCANCODE_5,		0x02 }, // 5
-	{ SDL_SCANCODE_7,		0x03 }, // 7
-	{ SDL_SCANCODE_9,		0x04 }, // 9
-	//{ SDL_SCANCODE_+		0x05 },	// PLUS
-	//{ SDL_SCANCODE_font		0x06 },	// FONT
-	{ SDL_SCANCODE_BACKSPACE,	0x07 },	// DEL
-	//{ SDL_SCANCODE_// UNKNOWN KEY?			0x10	//
-	{ SDL_SCANCODE_W,		0x11 }, // W
-	{ SDL_SCANCODE_R,		0x12 }, // R
-	{ SDL_SCANCODE_Y,		0x13 }, // Y
-	{ SDL_SCANCODE_I,		0x14 }, // I
-	{ SDL_SCANCODE_P,		0x15 }, // P
-	//{ SDL_SCANCODE_STAR // *
-	{ SDL_SCANCODE_RETURN,		0x17 }, // RETURN
-	{ SDL_SCANCODE_LCTRL,		0x20 }, // CTRL, only the left ctrl is mapped as vic-20 ctrl ...
-	{ SDL_SCANCODE_A,		0x21 }, // A
-	{ SDL_SCANCODE_D,		0x22 }, // D
-	{ SDL_SCANCODE_G,		0x23 }, // G
-	{ SDL_SCANCODE_J,		0x24 }, // J
-	{ SDL_SCANCODE_L,		0x25 }, // L
-	{ SDL_SCANCODE_SEMICOLON,	0x26 }, // ;
-	{ SDL_SCANCODE_RIGHT, 0x27 }, { SDL_SCANCODE_LEFT, 0x27 | 8 },	// CURSOR RIGHT, _SHIFTED_: CURSOR LEFT!
-	{ SDL_SCANCODE_END,		0x30 }, // RUN/STOP !! we use PC key 'END' for this!
-	{ SDL_SCANCODE_LSHIFT,		0x31 }, // LEFT SHIFT
-	{ SDL_SCANCODE_X,		0x32 }, // X
-	{ SDL_SCANCODE_V,		0x33 }, // V
-	{ SDL_SCANCODE_N,		0x34 }, // N
-	{ SDL_SCANCODE_COMMA,		0x35 }, // ,
-	{ SDL_SCANCODE_SLASH,		0x36 }, // /
-	{ SDL_SCANCODE_DOWN, 0x37 }, { SDL_SCANCODE_UP, 0x37 | 8 }, // CURSOR DOWN, _SHIFTED_: CURSOR UP!
-	{ SDL_SCANCODE_SPACE,		0x40 }, // SPACE
-	{ SDL_SCANCODE_Z,		0x41 }, // Z
-	{ SDL_SCANCODE_C,		0x42 }, // C
-	{ SDL_SCANCODE_B,		0x43 }, // B
-	{ SDL_SCANCODE_M,		0x44 }, // M
-	{ SDL_SCANCODE_PERIOD,		0x45 }, // .
-	{ SDL_SCANCODE_RSHIFT,		0x46 }, // RIGHT SHIFT
-	{ SDL_SCANCODE_F1, 0x47 }, { SDL_SCANCODE_F2, 0x47 | 8 }, // F1, _SHIFTED_: F2!
-	{ SDL_SCANCODE_LALT, 0x50 }, { SDL_SCANCODE_RALT, 0x50 }, // COMMODORE (may fav key!), PC sucks, no C= key :) - we map left and right ALT here ...
-	{ SDL_SCANCODE_S,		0x51 }, // S
-	{ SDL_SCANCODE_F,		0x52 }, // F
-	{ SDL_SCANCODE_H,		0x53 }, // H
-	{ SDL_SCANCODE_K,		0x54 }, // K
-	{ SDL_SCANCODE_APOSTROPHE,	0x55 },	// :    we map apostrophe here
-	{ SDL_SCANCODE_EQUALS,		0x56 }, // =
-	{ SDL_SCANCODE_F3, 0x57 }, { SDL_SCANCODE_F4, 0x57 | 8 }, // F3, _SHIFTED_: F4!
-	{ SDL_SCANCODE_Q,		0x60 }, // Q
-	{ SDL_SCANCODE_E,		0x61 }, // E
-	{ SDL_SCANCODE_T,		0x62 }, // T
-	{ SDL_SCANCODE_U,		0x63 }, // U
-	{ SDL_SCANCODE_O,		0x64 }, // O
-	//{ SDL_SCANCODE_// @
-	// UNKNOWN KEY?!?! 0x66
-	{ SDL_SCANCODE_F5, 0x67 }, { SDL_SCANCODE_F6, 0x67 | 8 }, // F5, _SHIFTED_: F6!
-	{ SDL_SCANCODE_2,		0x70 }, // 2
-	{ SDL_SCANCODE_4,		0x71 }, // 4
-	{ SDL_SCANCODE_6,		0x72 }, // 6
-	{ SDL_SCANCODE_8,		0x73 }, // 8
-	{ SDL_SCANCODE_0,		0x74 }, // 0
-	{ SDL_SCANCODE_MINUS,		0x75 }, // -
-	{ SDL_SCANCODE_HOME,		0x76 }, // HOME
-	{ SDL_SCANCODE_F7, 0x77 }, { SDL_SCANCODE_F8, 0x77 | 8 }, // F7, _SHIFTED_: F8!
-	// -- the following key definitions are not really part of the original VIC20 kbd matrix, we just *emulate* things this way!!
-	// Note: the exact "virtual" kbd matrix positions are *important* and won't work otherwise (arranged to be used more positions with one bit mask and, etc).
-	{ SDL_SCANCODE_KP_5,		0x85 },	// for joy FIRE  we map PC num keypad 5
-	{ SDL_SCANCODE_KP_0,		0x85 },	// PC num keypad 0 is also the FIRE ...
-	{ SDL_SCANCODE_RCTRL,		0x85 }, // and RIGHT controll is also the FIRE ... to make Sven happy :)
-	{ SDL_SCANCODE_KP_8,		0x82 },	// for joy UP    we map PC num keypad 8
-	{ SDL_SCANCODE_KP_2,		0x83 },	// for joy DOWN  we map PC num keypad 2
-	{ SDL_SCANCODE_KP_4,		0x84 },	// for joy LEFT  we map PC num keypad 4
-	{ SDL_SCANCODE_KP_6,		0x87 },	// for joy RIGHT we map PC num keypad 6
-	{ SDL_SCANCODE_ESCAPE,		0x81 },	// RESTORE key
+
+/* Primo for real does not have the notion if "keyboard matrix", well or we
+   can say it has 1*64 matrix (not like eg C64 with 8*8). Since the current
+   Xemu HID structure is more about a "real" matrix with "sane" dimensions,
+   I didn't want to hack it over, instead we use a more-or-less artificial
+   matrix, and we map that to the Primo I/O port request on port reading.
+   Since, HID assumes the high nibble of the "position" is the "row" and
+   low nibble can be only 0-7 we have values like:
+   $00 - $07, $10 - $17, $20 - $27, ...
+   ALSO: Primo uses bit '1' for pressed, so we also invert value in
+   the port read function above.
+*/
+static const struct KeyMapping primo_key_map[] = {
+	{ SDL_SCANCODE_Y,	0x00 },	// scan 0 Y
+	{ SDL_SCANCODE_UP,	0x01 },	// scan 1 UP-ARROW
+	{ SDL_SCANCODE_S,	0x02 },	// scan 2 S
+	{ SDL_SCANCODE_LSHIFT,	0x03 },	{ SDL_SCANCODE_RSHIFT,  0x03 }, // scan 3 SHIFT
+	{ SDL_SCANCODE_E,	0x04 },	// scan 4 E
+	//{ SDL_SCANCODE_UPPER,	0x05 },	// scan 5 UPPER
+	{ SDL_SCANCODE_W,	0x06 },	// scan 6 W
+	{ SDL_SCANCODE_LCTRL,	0x07 },	// scan 7 CTR
+	{ SDL_SCANCODE_D,	0x10 },	// scan 8 D
+	{ SDL_SCANCODE_3,	0x11 },	// scan 9 3 #
+	{ SDL_SCANCODE_X,	0x12 },	// scan 10 X
+	{ SDL_SCANCODE_2,	0x13 },	// scan 11 2 "
+	{ SDL_SCANCODE_Q,	0x14 },	// scan 12 Q
+	{ SDL_SCANCODE_1,	0x15 },	// scan 13 1 !
+	{ SDL_SCANCODE_A,	0x16 },	// scan 14 A
+	{ SDL_SCANCODE_DOWN,	0x17 },	// scan 15 DOWN-ARROW
+	{ SDL_SCANCODE_C,	0x20 },	// scan 16 C
+	//{ SDL_SCANCODE_----,	0x21 },	// scan 17 ----
+	{ SDL_SCANCODE_F,	0x22 },	// scan 18 F
+	//{ SDL_SCANCODE_----,	0x23 },	// scan 19 ----
+	{ SDL_SCANCODE_R,	0x24 },	// scan 20 R
+	//{ SDL_SCANCODE_----,	0x25 },	// scan 21 ----
+	{ SDL_SCANCODE_T,	0x26 },	// scan 22 T
+	{ SDL_SCANCODE_7,	0x27 },	// scan 23 7 /
+	{ SDL_SCANCODE_H,	0x30 },	// scan 24 H
+	{ SDL_SCANCODE_SPACE,	0x31 },	// scan 25 SPACE
+	{ SDL_SCANCODE_B,	0x32 },	// scan 26 B
+	{ SDL_SCANCODE_6,	0x33 },	// scan 27 6 &
+	{ SDL_SCANCODE_G,	0x34 },	// scan 28 G
+	{ SDL_SCANCODE_5,	0x35 },	// scan 29 5 %
+	{ SDL_SCANCODE_V,	0x36 },	// scan 30 V
+	{ SDL_SCANCODE_4,	0x37 },	// scan 31 4 $
+	{ SDL_SCANCODE_N,	0x40 },	// scan 32 N
+	{ SDL_SCANCODE_8,	0x41 },	// scan 33 8 (
+	{ SDL_SCANCODE_Z,	0x42 },	// scan 34 Z
+	//{ SDL_SCANCODE_PLUS,	0x43 },	// scan 35 + ?
+	{ SDL_SCANCODE_U,	0x44 },	// scan 36 U
+	{ SDL_SCANCODE_0,	0x45 },	// scan 37 0
+	{ SDL_SCANCODE_J,	0x46 },	// scan 38 J
+	//{ SDL_SCANCODE_>,	0x47 },	// scan 39 > <
+	{ SDL_SCANCODE_L,	0x50 },	// scan 40 L
+	{ SDL_SCANCODE_MINUS,	0x51 },	// scan 41 - i
+	{ SDL_SCANCODE_K,	0x52 },	// scan 42 K
+	{ SDL_SCANCODE_PERIOD,	0x53 },	// scan 43 . :
+	{ SDL_SCANCODE_M,	0x54 },	// scan 44 M
+	{ SDL_SCANCODE_9,	0x55 },	// scan 45 9 ;
+	{ SDL_SCANCODE_I,	0x56 },	// scan 46 I
+	{ SDL_SCANCODE_COMMA,	0x57 },	// scan 47 ,
+	//{ SDL_SCANCODE_U",	0x60 },	// scan 48 U"
+	{ SDL_SCANCODE_APOSTROPHE,	0x61 },	// scan 49 ' #
+	{ SDL_SCANCODE_P,	0x62 },	// scan 50 P
+	//{ SDL_SCANCODE_u',	0x63 },	// scan 51 u' u"
+	{ SDL_SCANCODE_O,	0x64 },	// scan 52 O
+	{ SDL_SCANCODE_HOME,	0x65 },	// scan 53 CLS
+	//{ SDL_SCANCODE_----,	0x66 },	// scan 54 ----
+	{ SDL_SCANCODE_RETURN,	0x67 },	// scan 55 RETURN
+	//{ SDL_SCANCODE_----,	0x70 },	// scan 56 ----
+	{ SDL_SCANCODE_LEFT,	0x71 },	// scan 57 LEFT-ARROW
+	//{ SDL_SCANCODE_E',	0x72 },	// scan 58 E'
+	//{ SDL_SCANCODE_o',	0x73 },	// scan 59 o'
+	//{ SDL_SCANCODE_A',	0x74 },	// scan 60 A'
+	{ SDL_SCANCODE_RIGHT,	0x75 },	// scan 61 RIGHT-ARROW
+	//{ SDL_SCANCODE_O:,	0x76 },	// scan 62 O:
+	{ SDL_SCANCODE_ESCAPE,	0x77 },	// scan 63 BRK
 	// **** this must be the last line: end of mapping table ****
 	{ 0, 0xFF }
 };
@@ -172,7 +173,7 @@ static const struct KeyMapping key_map[] = {
 
 void clear_emu_events ( void )
 {
-	keyboard_state = 0;
+	hid_reset_events(1);
 }
 
 
@@ -187,20 +188,7 @@ static void emulate_keyboard ( SDL_Scancode key, int pressed )
 		if (pressed)
 			exit(0);
 	} else {
-		const struct KeyMapping *map = key_map;
-		while (map->pos != 0xFF) {
-			if (map->scan == key) {
-				if (pressed)
-					keyboard_state |= 1 << (map->pos & 0x3F);
-				else
-					keyboard_state &= ~(1 << (map->pos & 0x3F));
-				//if (map->pos & 8)		// shifted key emu?
-				//	KBD_SET_KEY(0x31, pressed);	// maintain the shift key on VIC20!
-				//KBD_SET_KEY(map->pos, pressed);
-				break;	// key found, end.
-			}
-			map++;
-		}
+		hid_key_event(key, pressed);
 	}
 }
 
@@ -241,7 +229,6 @@ static void update_emulator ( void )
 				case SDL_QUIT:		// ie: someone closes the SDL window ...
 					exit(0);
 					break;
-				/* --- keyboard events --- */
 				case SDL_KEYDOWN:	// key is pressed (down)
 				case SDL_KEYUP:		// key is released (up)
 					// make sure that key event is for our window, also that it's not a releated event by long key presses (repeats should be handled by the emulated machine's KERNAL)
@@ -281,6 +268,11 @@ int main ( int argc, char **argv )
 		NULL			// no emulator specific shutdown function
 	))
 		return 1;
+	hid_init(
+		primo_key_map,
+		VIRTUAL_SHIFT_POS,
+		SDL_DISABLE		// no joystick HID events
+	);
 	/* Intialize memory and load ROMs */
 	memset(memory, 0xFF, sizeof memory);
 	if (emu_load_file(ROM_NAME, memory, 0x4001) != 0x4000)
@@ -292,10 +284,10 @@ int main ( int argc, char **argv )
 	emu_timekeeping_start();	// we must call this once, right before the start of the emulation
 	for (;;) { // our emulation loop ...
 		cycles += z80ex_step();
-		if (cycles >= CPU_CLOCK / 50) {
+		if (cycles >= CLOCKS_PER_FRAME) {
 			update_emulator();
 			frameskip = !frameskip;
-			cycles -= CPU_CLOCK / 50;
+			cycles -= CLOCKS_PER_FRAME;
 		}
 	}
 	return 0;
