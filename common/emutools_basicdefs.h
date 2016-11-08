587b8b4a7deb7098b88d92a1551b30767074fca0
LGB <lgblgblgb@gmail.com>
2016-11-08 17:23:28 +0100

    Trying to fix PATH_MAX on OSX /w limits.h

--- common/emutools_basicdefs.h
+++ common/emutools_basicdefs.h
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #include <stdio.h>
 #include <SDL_types.h>
 #include <SDL_endian.h>
+#include <limits.h>
 
 #define USE_REGPARM
 
