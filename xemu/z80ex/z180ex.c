6cc213b265c76892643f8fe86a651e745595417c
LGB <lgblgblgb@gmail.com>
2020-02-23 23:01:07 +0100

    ...

--- xemu/z80ex/z180ex.c
+++ xemu/z80ex/z180ex.c
@@ -5,6 +5,8 @@
    Additional quirky Z180 emulation for Z80Ex. You should read
    file README about the (lack of ...) features.
 
+   FIXME: This must be integrated into opcodes_ed.c!
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
