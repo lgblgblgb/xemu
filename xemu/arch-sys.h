da007673beb89b22ee4aac3d755d26d630164521
lgblgblgb <lgblgblgb@gmail.com>
2020-08-27 15:41:41 +0200

    MACOS: workaround Mac+SDL bug on memset_pattern4 prototype

--- xemu/arch-sys.h
+++ xemu/arch-sys.h
@@ -93,3 +93,12 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #	undef	_FILE_OFFSET_BITS
 #endif
 #define	_FILE_OFFSET_BITS	64
+
+#ifdef XEMU_ARCH_OSX
+	// MacOS and/or SDL bug: even if this prototype is in string.h system header in MacOS,
+	// somehow it does not work even if string.h _IS_ included. So I have to create my
+	// own prototype for this function here :-O For the prototype, we also need the stddef.h
+	// though, for the "size_t".
+#	include <stddef.h>
+	void memset_pattern4(void *__b, const void *__pattern4, size_t __len);
+#endif
