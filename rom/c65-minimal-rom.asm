bfdcda7f32d118aae57c6354a29ce0c69c45d326
LGB (Gabor Lenart) <lgblgblgb@gmail.com>
2021-09-22 19:55:56 +0200

    CORE: build sys updates from branch 'merger'

---
+++ rom/c65-minimal-rom.asm
@@ -0,0 +1,31 @@
+; This is a very simple C65 (or MEGA65) ROM, which is totally unusable,
+; just enough to display a message for Xemu users, so they get to know
+; what they should do. This is not a "normal" ROM either by layout,
+; but has "ID points". Only useful really, inside Xemu.
+;
+; Copyright (C)2021 LGB (Gábor Lénárt) <lgblgblgb@gmail.com>
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+.SETCPU	"4510"
+.ORG	0
+
+; "C65 ROM version" style of string, since it's used by Xemu to figure out
+; the needed DMA revision for the given ROM.
+
+.BYTE "V911111"
+
+
+
