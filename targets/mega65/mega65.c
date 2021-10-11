13f77bc7c263c7eb3674a70e17e40d1ba032c1db
LGB (Gabor Lenart) <lgblgblgb@gmail.com>
2021-10-11 23:59:11 +0200

    MEGA65: use ext default.d81 if found
    Common user complain: the default used "drive 8" disk image is inside
    the SD-card image, thus very hard to deal with. This commit tries to
    ease that: if no -8 option was used _AND_ a file default.d81 can be
    found (with the right size of a D81) in the SDL-pref-dir, then it's
    used by default. The user however still needs to put there a valid
    D81 there, though.

--- targets/mega65/mega65.c
+++ targets/mega65/mega65.c
@@ -375,6 +375,25 @@ static void mega65_init ( void )
 	// This is a separated step, to be able to call refill_memory_from_preinit_cache() later as well, in case of a "deep reset" functionality is needed for Xemu (not just CPU/hw reset),
 	// without restarting Xemu for that purpose.
 	refill_memory_from_preinit_cache();
+	// If we have no -8 option given, but we found a suitable disk image in the pref-dir,
+	// with the desired name, let's use that! In this way, it may cure some complains,
+	// that the default disk is "inside" the SD-card image which is hard to deal with.
+	if (!configdb.disk8) {
+		static const char default_d81_fn[] = "default.d81";
+		char *fn = xemu_malloc(strlen(sdl_pref_dir) + strlen(default_d81_fn) + 1);
+		sprintf(fn, "%s%s", sdl_pref_dir, default_d81_fn);
+		off_t size = xemu_safe_file_size_by_name(fn);
+		if (size != OFF_T_ERROR) {
+			if (size == (off_t)D81_SIZE) {
+				DEBUGPRINT("DISK: using external default disk image, since without -8 we found: %s" NL, fn);
+				configdb.disk8 = fn;
+			} else {
+				ERROR_WINDOW("Found: %s\nfor default external disk image,\nbut it has wrong size", fn);
+				free(fn);
+			}
+		} else
+			free(fn);
+	}
 	// *** Image file for SDCARD support
 	if (sdcard_init(configdb.sdimg, configdb.disk8, configdb.virtsd) < 0)
 		FATAL("Cannot find SD-card image (which is a must for MEGA65 emulation): %s", configdb.sdimg);
