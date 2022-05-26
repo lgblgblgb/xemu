a2d7136aff8434f42f761dc34f1078b3577cc0ca
LGB (Gabor Lenart) <lgblgblgb@gmail.com>
2022-05-27 00:04:22 +0200

    CLCD: fine-tune direct PRG load/save

--- targets/clcd/commodore_lcd.c
+++ targets/clcd/commodore_lcd.c
@@ -75,7 +75,6 @@ static struct {
 	int phase;
 	Uint8 data[55294];
 	int size;
-	int basic_size;
 } prg_inject;
 
 static const Uint8 init_lcd_palette_rgb[6] = {
@@ -466,6 +465,7 @@ static int prg_relink ( Uint8 *data, int target_addr, int source_addr, const int
 		i += 2;	// jump over nextptr
 		if (!nextptr)
 			break;	// end of program
+		//DEBUGPRINT("BASIC LINE: %d" NL, data[i] + data[i + 1] * 256);
 		i += 2;	// jump over basic line number word
 		for (int linelength = 0;;) {
 			if (i >= size_limit)
@@ -475,9 +475,11 @@ static int prg_relink ( Uint8 *data, int target_addr, int source_addr, const int
 			if (++linelength >= 255)	// FIXME: max line length!
 				return -1;
 		}
-		source_addr += i - ptrpos;
-		if (nextptr != source_addr)
-			return -1;
+		if (source_addr >= 0) {
+			source_addr += i - ptrpos;
+			if (nextptr != source_addr)
+				return -1;
+		}
 		if (target_addr >= 0) {
 			target_addr += i - ptrpos;
 			if (nextptr != target_addr) {
@@ -502,55 +504,99 @@ static int prg_load_prepare_inject ( const char *file_name, int new_address )
 	prg_inject.size = xemu_load_file(file_name, prg_inject.data, 4, sizeof(prg_inject.data) - 4, "Cannot load program");
 	if (prg_inject.size <= 0)
 		return -1;
-	prg_inject.basic_size = prg_inject.size - 2;
-	const int old_address = prg_inject.data[0] | (prg_inject.data[1] << 8);
-	DEBUGPRINT("PRG: program \"%s\" load_addr=$%04X, new_load_addr=$%04X, size=%d, basic_size=%d" NL, file_name, old_address, new_address, prg_inject.size, prg_inject.basic_size);
-	int real_size;
-	const int relink_status = prg_relink(prg_inject.data + 2, new_address, old_address, prg_inject.basic_size, &real_size);
-	if (relink_status < 0) {
-		ERROR_WINDOW("Invalid PRG, bad link structure");
+	if (prg_inject.size <= 4) {
+		ERROR_WINDOW("PRG is empty");
 		return -1;
 	}
-	DEBUGPRINT("PRG: relink points=%d, size_from_linking=%d" NL, relink_status, real_size);
-	if (real_size != prg_inject.basic_size)
-		DEBUGPRINT("PRG: warning, program contains extra non-BASIC %d bytes" NL, real_size - prg_inject.basic_size);
+	const int old_address = prg_inject.data[0] + (prg_inject.data[1] << 8);
+	DEBUGPRINT("PRG: program \"%s\" load_addr=$%04X, new_load_addr=$%04X, size=%d" NL, file_name, old_address, new_address, prg_inject.size);
+	int linked_size;
+	const int relink_status = prg_relink(prg_inject.data + 2, new_address, -1, prg_inject.size - 2, &linked_size);
+	if (relink_status < 0) {
+		ERROR_WINDOW("Invalid PRG, bad BASIC link chain");
+		return -1;
+	}
+	DEBUGPRINT("PRG: relink points=%d, size_from_linking=%d" NL, relink_status, linked_size);
+	const int tail = prg_inject.size - 2 - linked_size;
+	if (tail < 0) {	// technically it shouldn't happen ever, since prg_relink above should return then with relink_status < 0
+		ERROR_WINDOW("Too short?!");
+		return -1;
+	}
+	if (tail)
+		DEBUGPRINT("PRG: PRG has non-BASIC tail of %d byte(s)" NL, tail);
+	else
+		DEBUGPRINT("PRG: PRG has no non-BASIC tail" NL);
 	return 0;
 }
 
 
-static int prg_load_do_inject ( void )
+static int prg_load_do_inject ( const int address )
 {
 	// TODO: find out some way to detect if BASIC is unning at the moment, and if we're in screen-edit mode (not like running a program or something)
-	int addr = BASIC_START;
-	if (memory[0x65] != (addr & 0xFF) || memory[0x66] != (addr >> 8)) {
-		ERROR_WINDOW("Could not inject PRG into memory:\nwrong BASIC start address in ZP: $%04X (should be: $%04X)", memory[0x65] + (memory[0x66] << 8), addr);
+	if (memory[0x65] != (address & 0xFF) || memory[0x66] != (address >> 8)) {
+		ERROR_WINDOW("Could not inject PRG into memory:\nwrong BASIC start address in ZP: $%04X (should be: $%04X)", memory[0x65] + (memory[0x66] << 8), address);
 		return -1;
 	}
-	// As a kind of test, we use relink on the memory itself first, if there is something there.
 	const int memtop = memory[0x71] + (memory[0x72] << 8);
+#if 0
 	int previous_size = -1;
-	const int prev_status = prg_relink(memory + BASIC_START, -1, BASIC_START, memtop - BASIC_START, &previous_size);
+	// NOTE: this is *NOT* relink the program. It *CHECKS* links of the program in the memory BEFORE injecting the new program which overwrites memory.
+	// The only intent of this step is try to be sure that we're in BASIC mode at all hoping that "no answer" would mean, these checks fail.
+	const int prev_status = prg_relink(memory + address, -1, address, memtop - address, &previous_size);
 	DEBUGPRINT("PRG: previous status: %d, previous size: %d" NL, prev_status, previous_size);
-	const int last_addr = addr + prg_inject.size + 0x100; 	// 0x100: margin of error for now ;) TODO
+#endif
+	const int last_addr = address + prg_inject.size + 0x100; 	// 0x100: margin of error for now ;) TODO
 	if (last_addr >= memtop) {
 		ERROR_WINDOW("Program is too large to fit into the available BASIC memory\nMEMTOP = $%04X, last_load_byte = $%04X", memtop, last_addr);
 		return -1;
 	}
-	memory[0x1000] = 0;
-	// memset(memory + 0x1000, 0, 0x8000); // FIXME: what is this?
-	memcpy(memory + BASIC_START, prg_inject.data + 2, prg_inject.size - 2);
-	//memset(memory + BASIC_START + prg_inject.size - 2, 0, 4);
-	memory[0x65] = addr & 0xFF;
-	memory[0x66] = addr >> 8;
-	addr += prg_inject.basic_size;
-	memory[0x67] = addr & 0xFF;
-	memory[0x68] = addr >> 8;
-	memory[0x69] = addr & 0xFF;
-	memory[0x6A] = addr >> 8;
-	memory[0x6B] = addr & 0xFF;
-	memory[0x6C] = addr >> 8;
+	memory[address - 1] = 0;	// I *think* that byte should be zero. I'm not absolutely sure though.
+	memcpy(memory + address, prg_inject.data + 2, prg_inject.size - 2);
+	memory[0x65] = address & 0xFF;
+	memory[0x66] = address >> 8;
+	const int end_address = address + prg_inject.size - 2;
+	memory[0x67] = end_address & 0xFF;
+	memory[0x68] = end_address >> 8;
+	memory[0x69] = end_address & 0xFF;
+	memory[0x6A] = end_address >> 8;
+	memory[0x6B] = end_address & 0xFF;
+	memory[0x6C] = end_address >> 8;
 	memory[0x6D] = memory[0x71];
 	memory[0x6E] = memory[0x72];
+	return 0;
+}
+
+
+static int prg_save_prepare_store ( const int address )
+{
+	prg_inject.phase = 0;
+	if (memory[0x65] != (address & 0xFF) || memory[0x66] != (address >> 8)) {
+		ERROR_WINDOW("SAVE error: wrong start addr ($%02X%02X) of BASIC ZP (should be: $%04X)", memory[0x66], memory[0x65], address);
+		return -1;
+	}
+	const int memtop = memory[0x67] + (memory[0x68] << 8);
+	const int size = memtop - address;	// size of the current program, without the load address
+	if (size < 2 || size >= sizeof(prg_inject.data) - 2) {
+		ERROR_WINDOW("SAVE error: Bad end of prg ptr in BASIC ZP");
+		return -1;
+	}
+	if (size <= 4) {
+		ERROR_WINDOW("SAVE error: no BASIC PRG in memory");
+		return -1;
+	}
+	// use the relink functionality to check validity of memory
+	int linked_size;
+	const int ret = prg_relink(memory + address, -1, address, size, &linked_size);
+	if (ret) {
+		ERROR_WINDOW("SAVE error: in-memory program has invalid link structure, retcode: %d", ret);
+		return -1;
+	}
+	DEBUGPRINT("SAVE: size from pointers = %d, size from link-check = %d" NL, size, linked_size);
+	// hijack prg_inject.data for our purposes here ...
+	prg_inject.data[0] = address & 0xFF;
+	prg_inject.data[1] = address >> 8;
+	memcpy(prg_inject.data + 2, memory + address, size);
+	prg_inject.size = size + 2;
 	return 0;
 }
 
@@ -566,7 +612,7 @@ static void update_emulator ( void )
 		) {
 			prg_inject.phase = 2;
 			DEBUGPRINT("BASIC: startup screen detected, injecting loaded basic program!" NL);
-			prg_load_do_inject();
+			prg_load_do_inject(BASIC_START);
 			memory[0xA01] = 'R' - 'A' + 1;
 			memory[0xA02] = 'U' - 'A' + 1;
 			memory[0xA03] = 'N' - 'A' + 1;
@@ -643,26 +689,14 @@ static void ui_load_basic_prg ( void )
 		if (prg_load_prepare_inject(fnbuf, BASIC_START)) {
 			return;
 		}
-		prg_load_do_inject();
+		prg_load_do_inject(BASIC_START);
 	}
 }
 
 static void ui_save_basic_prg ( void )
 {
-	if (memory[0x65] != (BASIC_START & 0xFF) || memory[0x66] != (BASIC_START << 8)) {
-		ERROR_WINDOW("Wrong start addr of BASIC ZP");
+	if (prg_save_prepare_store(BASIC_START))
 		return;
-	}
-	int size = memory[0x67] + (memory[0x68] << 8) - BASIC_START;	// size of the current program, without the load address
-	if (size < 2 || size >= sizeof(prg_inject.data) - 2) {
-		ERROR_WINDOW("Bad end of prg ptr in BASIC ZP");
-		return;
-	}
-	// hijack prg_inject.data for our purposes here ...
-	prg_inject.data[0] = BASIC_START & 0xFF;
-	prg_inject.data[1] = BASIC_START >> 8;
-	memcpy(prg_inject.data + 2, memory + BASIC_START, size);
-	size += 2;
 	char fnbuf[PATH_MAX + 1];
 	if (!xemugui_file_selector(
 		XEMUGUI_FSEL_SAVE | XEMUGUI_FSEL_FLAG_STORE_DIR,
@@ -671,7 +705,7 @@ static void ui_save_basic_prg ( void )
 		fnbuf,
 		sizeof fnbuf
 	)) {
-		xemu_save_file(fnbuf, prg_inject.data, size, "Cannot save BASIC PRG");
+		xemu_save_file(fnbuf, prg_inject.data, prg_inject.size, "Cannot save BASIC PRG");
 	}
 }
 
