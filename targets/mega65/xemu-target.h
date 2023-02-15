33d70ae691377782ff8b7995baeb0c63899bcc38
lgblgblgb <lgblgblgb@gmail.com>
2023-02-15 11:26:36 +0100

    M65/C65: workaround for hung on fast-IEC access
    C65 and MEGA65 ROM tries fast-seriel IEC protocol when trying to access
    IEC devices. This leads a hung in Xemu, as fast serial protocol uses
    the SDR (Serial Data Register) for hardware serialization, utilizing the
    CIA feature to signal IRQ when SDR is full/empty. Though IEC is not
    emulated by Xemu at all, having a hung is very annoying. Thus there is
    some macro now in xemu/cia6526.c which simply can set bit 3 of ICR reg
    on SDR register write, C65 and MEGA65 emulators set the OUT macro of
    those, which seems to workaround the problem at least, for now ...

--- targets/mega65/xemu-target.h
+++ targets/mega65/xemu-target.h
@@ -84,3 +84,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #define XEMU_CONFIGDB_SUPPORT
 #define XEMU_OSD_SUPPORT
+
+// Workaround for hang causes by trying fast-seriel-IEC by ROM
+// These defines are used by xemu/cia6526.c
+//#define CIA_IN_SDR_SETS_ICR_BIT3
+#define CIA_OUT_SDR_SETS_ICR_BIT3
