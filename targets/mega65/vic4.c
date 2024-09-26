ef9be42a0a12de8cb37642e231f809edd5da17d4
RetroCogs <45956643+RetroCogs@users.noreply.github.com>
2024-09-26 14:51:55 -0700

    Fixing CHRCOUNT for being 10 bits
    Issue has been found and fixed by @RetroCogs
    
    Sync with next from a00858f01054931e69840e15d8d73c2deed69057

--- targets/mega65/vic4.c
+++ targets/mega65/vic4.c
@@ -361,7 +361,11 @@ static void vic4_interpret_legacy_mode_registers ( void )
 	vic4_update_vertical_borders();
 
 	Uint8 width = REG_H640 ? 80 : 40;
-	REG_CHRCOUNT = width;
+
+	// Set all 10 bits of ChrCount
+	vic_registers[0x5e] = width;
+	vic_registers[0x63] &= 0xCF; // clearing bits 4 and 5, being MSB of CHRCOUNT
+
 	SET_LINESTEP_BYTES(width);	// * (REG_16BITCHARSET ? 2 : 1));
 
 	REG_SCRNPTR_B0 = 0;
