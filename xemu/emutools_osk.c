3959ed667b3952f8054c4188a75770d147c11509
LGB (Gabor Lenart) <lgblgblgb@gmail.com>
2025-05-30 15:07:21 +0200

    MEGA65: missing included header in OSK implementation

--- xemu/emutools_osk.c
+++ xemu/emutools_osk.c
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #include "xemu/emutools.h"
 #include "xemu/emutools_osk.h"
 #include "xemu/emutools_hid.h"
+#include <string.h>
 
 // Do not change these, identifiers
 #define OSK_KEY_HIDE	-100
