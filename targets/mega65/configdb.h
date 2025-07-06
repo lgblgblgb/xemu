b53140be9c660b93abaaf61056939264b3338328
LGB (Gabor Lenart) <lgblgblgb@gmail.com>
2025-07-06 13:23:53 +0200

    MEGA65: better cartridge + new CRT support #380
    Still preliminary, no proper check of CRT files at every level, also no
    bank support, no ROM/RAM support (everything is treated as ROM).
    
    [DEPLOYMENT]

--- targets/mega65/configdb.h
+++ targets/mega65/configdb.h
@@ -97,7 +97,7 @@ struct configdb_st {
 	// $60/$61/$62 = qmtecha100t/qmtecha200t/qmtecha325t, $21/$22=megaphoner1/megaphoner4
 	int	mega65_model;
 	int	colour_effect;
-	char	*cartbin8000;
+	char	*cart;
 	int	hicked;
 	int	prgmode;
 	int	rtc_hour_offset;
